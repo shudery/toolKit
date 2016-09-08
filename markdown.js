b6f315dc2d8c30855945de8b5489838a796a2c92
daguo <shudery@foxmail.com>
2016-09-09 02:57:00 +0800

    init

---
+++ markdown.js
@@ -0,0 +1,56 @@
+/**
+ * showAsmd 简单的markdown语法转换成html标签工具，标签类型为[相应的标签名-md]
+ */
+
+//目前支持的转换
+String.prototype.hash = {
+
+    img: /\[\!(.*?)\]\((.*?)\)/g, //[!alt](src)
+    a: /\[(?!\!)(.*?)\]\((.*?)\)/g, //[text](href)
+    code: /```([\s\S]*?)```/g, //```code```
+    strong: [/``?([\s\S]*?)``?/g, /(?!\n)\*([\s\S]*?)\*/g], //``strong``,*strong*
+    li: /\n[\*+\-]\s(.*)/g, //行首+*- 
+    ul: /(<li>.*<\/li>)(?!<li>)/g,
+    h1: /\n#\s(.*)/g,
+    h2: /\n##\s(.*)/g,
+    h3: /\n###\s(.*)/g,
+    h4: /\n####\s(.*)/g,
+    h5: /\n#####\s(.*)/g,
+    h6: /\n######\s(.*)/g,
+
+}
+if (String.prototype.showAsmd) {
+    console.log('showAsmd fn already exist!')
+}
+String.prototype.showAsmd = function() {
+
+    var str = this.toString();
+    if (typeof str !== 'string') {
+        console.log('expecting a string param!')
+        return;
+    }
+    var hash = this.hash;
+    for (var key in hash) {
+        var repText = '';
+        var keyRexExp = hash[key];
+        if (Object.prototype.toString.call(keyRexExp) === '[object RegExp]') {
+            switch (key) {
+                case 'a':
+                    repText = '<a class="a-md" href="$2">$1</a>';
+                    break;
+                case 'img':
+                    repText = '<img class="img-md" src="$2" alt="$1" />';
+                    break;
+                default:
+                    repText = '<' + key + ' class="' + key + '-md">$1</' + key + '>';
+
+            }
+            str = str.replace(keyRexExp, repText)
+        } else {
+            keyRexExp.forEach((val, index) => {
+                str = str.replace(val, '<' + key + ' class="' + key + '-md">$1</' + key + '>')
+            })
+        }
+    }
+    return str;
+}
---
+++ mdTojson.js
@@ -0,0 +1,111 @@
+var fs = require('fs');
+
+//生成对象json文件路径
+var targetPath = './datas.json'
+//md文件地址
+var rootPath = '../hexo/source/_posts/'
+//匹配正则
+var filesRex = /.*(\.md)/;
+
+
+//拿到根目录所有文件，匹配出md文件路径
+var allFilesPath = getFilesPath(rootPath);
+var postFilesPath = allFilesPath.reduce((pre, path) => {
+    filesRex.test(path) && pre.push(path);
+    return pre;
+}, []);
+var postFilesLen = postFilesPath.length;
+
+
+//任务开始前,如果当前根路径文件列表中有目标文件，则清空文件
+if (allFilesPath.indexOf(targetPath) !== -1) {
+    clearPostsObj(targetPath);
+}
+
+//开始读写转换任务
+eval(evalStr(postFilesPath, postsToJson));
+
+
+/**
+ * 拼接异步执行字符串
+ * @param  {Number}   num [文章数目]
+ * @param  {Function} fn  [将mark类型的文章提取成一个post对象]
+ * @return {String}       [异步执行的字符串]
+ */
+function evalStr(postFilesPath, fn) {
+    var str = 'postsToJson("' + postFilesPath[0] + '")'
+    for (var i = 1; i < postFilesLen; i++) {
+        str += '.then(() => postsToJson("' + postFilesPath[i] + '"))'
+    }
+    return str;
+}
+/**
+ * 将mark类型的文章提取成一个post对象
+ * @param  {Number} num [description]
+ * @return {Promise}     [description]
+ */
+function postsToJson(path) {
+    return new Promise(function(resolve, reject) {
+        var datas = fs.readFileSync(path, { encoding: 'utf8' })
+        var olddatas;
+        var noFileError;
+        //读旧文件，解析成数组，添加一个新的post对象，然后再转为字符串
+        try {
+            olddatas = fs.readFileSync(targetPath, { encoding: 'utf8' })
+        } catch (e) {
+            console.log('there are no file:' + targetPath);
+            noFileError = e;
+        }
+        var arr = olddatas ? JSON.parse(olddatas) : [];
+        arr.push(parsePost(datas));
+
+        //写入文件
+        fs.writeFileSync(targetPath, JSON.stringify(arr));
+        noFileError && console.log('create file:' + targetPath);
+        console.info(path + ' write to targetPath(' + targetPath + ') success!');
+        resolve();
+    })
+}
+/**
+ * 开始任务清空filename地址的文件内容
+ * @param  {[type]} filename [description]
+ * @return {[type]}          [description]
+ */
+function clearPostsObj(filename) {
+    fs.writeFileSync(filename, '')
+}
+/**
+ * 匹配提取md文件的相应字段到对象属性中
+ * @param  {[type]} postStr [description]
+ * @return {[type]}         [description]
+ */
+function parsePost(postStr) {
+    //hexo下post文章的md格式
+    var matchRe = /title:(.*?)\ndate:(.*?)\ntags:(.*?)\ndescription:(.*?)\n---\n([\s\S]*)/
+    var matchArr = postStr.match(matchRe);
+    return {
+        'header': matchArr[1],
+        'time': matchArr[2],
+        'tags': matchArr[3],
+        'description': matchArr[4],
+        'section': matchArr[5],
+    }
+}
+/**
+ * 遍历目录
+ * @param  {[type]} path [description]
+ * @return {[type]}      [description]
+ */
+function getFilesPath(path) {
+    var fileList = [];
+    (function searchPath(path) {
+        fs.readdirSync(path).forEach((item) => {
+            if (fs.statSync(path + item).isDirectory()) {
+                searchPath(path + item + '/');
+            } else {
+                fileList.push(path + item);
+            }
+        });
+    })(path)
+    return fileList;
+}
